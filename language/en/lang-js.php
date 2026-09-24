86ca7e77cad6ac0391825cfefff1e18ae81c26cf
huutrongdev93 <huutrong.it93@gmail.com>
2026-09-24 09:52:46 +0700

    [Fix] Add validation message for photo size in rating attachment

--- language/en/lang-js.php
+++ language/en/lang-js.php
@@ -1,4 +1,5 @@
 <?php
 return [
-    'rating.attach.extension' => 'The photo is not in the correct format'
+    'rating.attach.extension' => 'The photo is not in the correct format',
+    'rating.attach.size' => 'Your photo is larger than the allowed size'
 ];
